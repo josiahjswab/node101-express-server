27e4d14a2b7a09d66a7522c5cfb7ed394f522f3b
Josiah Swab <josiahjswab@gmail.com>
2019-12-16 10:16:51 -0800

    Piece of cake.

--- server/app.js
+++ server/app.js
@@ -1,10 +1,25 @@
 // import files and packages up here
-
+const express = require('express');
+const data = require('./data');
+const morgan = require('morgan');
 
 // create your express server below
-var app;
+const app = express();
+
 
 // add your routes and middleware below
+app.use(morgan('tiny'));
+
+app.get('/', function(req, res) {
+  console.log('I used to be an adventurer like you until I took an arrow to the knee.'),
+  res.send("hello world")
+});
+
+app.get('/data', function(req, res) {
+  console.log('Heres that thing you asked for.'),
+  res.send(data)
+});
+
 
 // finally export the express application
 module.exports = app;
